fb3ba7327bd6c17c68f9144b33960060ac4131a1
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-01-18 20:15:40 +0000

    Create ScriptableObject for containing Counter (model)

---
+++ UnityMVC/Assets/Scripts/UnityApp/CounterContainer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using MVCExample.Common;
+
+namespace MVCExample.UnityApp
+{
+    [CreateAssetMenu(menuName = "CounterContainer")]
+    public class CounterContainer : ScriptableObject
+    {
+        [SerializeField]
+        private int maxCount = 3;
+
+        public Counter Counter { get; private set; }
+
+        private void OnEnable()
+        {
+            Counter = new Counter(maxCount);
+        }
+    }
+}
