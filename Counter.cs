60dfb9d1e65d524eee21c42a35390bafd8ac598d
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-01-18 19:28:06 +0000

    Finish counter model

--- Counter.cs
+++ Counter.cs
@@ -7,6 +7,30 @@ namespace MVCExample.Common
         private int count;
         private readonly int maxCount;
 
+        public Counter(int maxCount)
+        {
+            if (maxCount < 1)
+                throw new ArgumentOutOfRangeException(
+                    "The maxCount argument must be > 0");
+            count = 0;
+            this.maxCount = maxCount;
+        }
 
+        public void IncrementCount()
+        {
+            count++;
+            if (count >= maxCount)
+            {
+                OnHitMaxCount();
+            }
+        }
+
+        protected void OnHitMaxCount()
+        {
+            HitMaxCount?.Invoke();
+            count = 0;
+        }
+
+        public event Action HitMaxCount;
     }
 }
