7bff95a26a87001e090079a69a9c55d2a8924c5e
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-01-18 20:44:34 +0000

    Finish Unity view

--- UnityMVC/Assets/Scripts/UnityApp/View.cs
+++ UnityMVC/Assets/Scripts/UnityApp/View.cs
@@ -8,19 +8,37 @@ namespace MVCExample.UnityApp
         [SerializeField]
         private CounterContainer counterContainer;
 
+        [SerializeField]
+        private float textVanishDuration = 1.0f;
+
+        private Text text;
+        private float lastTextTime = float.NegativeInfinity;
+
+        private readonly Color baseColor = Color.red;
+        private readonly Color noColor = new Color(1, 0, 0, 0);
+
         private void Awake()
         {
-
+            GameObject message = GameObject.Find("Message");
+            text = message.GetComponent<Text>();
+            text.color = noColor;
         }
 
         private void Update()
         {
-
+            float timeLeftToVanish =
+                lastTextTime + textVanishDuration - Time.time;
+            if (timeLeftToVanish > 0)
+            {
+                float t = timeLeftToVanish / textVanishDuration;
+                text.color = Color.Lerp(noColor, baseColor, t);
+            }
         }
 
         private void UpdateMessage()
         {
-
+            lastTextTime = Time.time;
+            Debug.Log("Hit max count!");
         }
 
         private void OnEnable()
