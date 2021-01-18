954f58924b383962eb7d695f6bf3b75b445a8a0c
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-01-18 21:21:01 +0000

    Implement console view

---
+++ ConsoleMVC/ConsoleApp/ConsoleView.cs
@@ -0,0 +1,54 @@
+using System;
+using MVCExample.Common;
+
+namespace MVCExample.ConsoleApp
+{
+    public class ConsoleView
+    {
+        private IController controller;
+
+        public ConsoleView(IController controller, Counter counter)
+        {
+            this.controller = controller;
+            counter.HitMaxCount += UpdateMessage;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("=== Welcome to the MVC Example ===");
+            Console.WriteLine();
+            Console.WriteLine(
+                "\tPress [Space] to increment counter, [Esc] to exit");
+        }
+
+        public void GetInput()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.Spacebar:
+                        controller.IncrementAction();
+                        break;
+                    case ConsoleKey.Escape:
+                        controller.Quit();
+                        break;
+                    default:
+                        Console.WriteLine("\tUnknown option!");
+                        break;
+                }
+            }
+        }
+
+        public void Finish()
+        {
+            Console.WriteLine("=== Bye! ===");
+        }
+
+        private void UpdateMessage()
+        {
+            Console.WriteLine("\t => Hit max presses!");
+        }
+    }
+}
