95f634c3e4bd66b2a679ddff66f79efbb133f727
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-01-18 20:24:34 +0000

    Implement Controller and add quit action

---
+++ UnityMVC/Assets/Scripts/UnityApp/Controller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace MVCExample.UnityApp
+{
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField]
+        private CounterContainer counterContainer;
+
+        public void IncrementAction()
+        {
+            counterContainer.Counter.IncrementCount();
+        }
+
+        public void Quit()
+        {
+#if UNITY_STANDALONE
+            Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
